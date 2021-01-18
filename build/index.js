72ac396370618f8ef7da10ae7e26457834206ccc
JosefMackuAmn <josef.zsd@seznam.cz>
2021-01-18 15:38:28 +0100

    Project setup

---
+++ build/index.js
@@ -0,0 +1,21 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var app = express_1.default();
+app.set('view engine', 'ejs');
+//app.use(bodyParser.json());
+app.use(express_1.default.static('public'));
+app.post('/email', function (req, res) {
+    // Send email
+    res.send('Not implemented yet');
+});
+app.get('/', function (req, res) {
+    res.render('index');
+});
+var PORT = process.env.PORT || 8080;
+app.listen(PORT, function () {
+    console.log('Listening on port 8080');
+});
---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+//app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.post('/email', (req, res) => {
+    // Send email
+    res.send('Not implemented yet');
+});
+
+app.get('/', (req, res) => {
+    res.render('index');
+})
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log('Listening on port 8080');
+});
